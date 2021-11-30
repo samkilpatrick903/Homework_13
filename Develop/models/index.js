90d1805c08f1f26ff5b4afd341d243c47dcad0aa
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-11-30 08:44:00 -0600

    updating index in models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,7 +6,8 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: "category_id"
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
 });
 // Categories have many Products
 Category.hasMany(Product, {
@@ -15,13 +16,18 @@ Category.hasMany(Product, {
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: "product_id"
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+    foreignKey: "product_id"
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: "tag_id"
+  through: {
+    model: ProductTag,
+    unique: false
+  },
 });
 
 module.exports = {
